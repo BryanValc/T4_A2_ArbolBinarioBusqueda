b9fba3ab6d5a054a5215b90a95ab2e45fe84e253
Bryan Valdez <bryan501th@gmail.com>
2020-11-19 16:54:40 -0600

    Se agrego todo lo visto en la sesion

---
+++ src/PruebaArbolBinarioBusqueda.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+
+interface Validacion{
+	Scanner input = new Scanner(System.in);
+	
+	public static int validacionNatural() {
+		int ret = 0;
+		boolean err = false;
+		do {
+			try {
+				ret = input.nextInt();
+			} catch (java.util.InputMismatchException e) {
+				System.out.println("entrada no valida, intente de nuevo:");
+				input.nextLine();
+				err=true;
+			}
+			if (ret>0) {
+				err=false;
+			}else {
+				System.out.println("solo números naturales");
+				err=true;
+			}
+		}while(err);
+		return ret;
+	}
+}
+
+
+class NodoArbol{
+	private NodoArbol nodoIzq;
+	private int dato;
+	private NodoArbol nodoDer;
+	
+	public NodoArbol getNodoIzq() {
+		return nodoIzq;
+	}
+	public void setNodoIzq(NodoArbol nodoIzq) {
+		this.nodoIzq = nodoIzq;
+	}
+	public int getDato() {
+		return dato;
+	}
+	public void setDato(int dato) {
+		this.dato = dato;
+	}
+	public NodoArbol getNodoDer() {
+		return nodoDer;
+	}
+	public void setNodoDer(NodoArbol nodoDer) {
+		this.nodoDer = nodoDer;
+	}
+	
+	@Override
+	public String toString() {
+		return "NodoArbol [nodoIzq=" + nodoIzq + ", dato=" + dato + ", nodoDer=" + nodoDer + "]";
+	}
+	
+}
+
+class ArbolBinarioBusqueda{
+	NodoArbol nodoRaiz;
+	
+	public ArbolBinarioBusqueda(){
+		nodoRaiz=null;
+	}
+	
+	
+}
+
+
+
+public class PruebaArbolBinarioBusqueda {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
